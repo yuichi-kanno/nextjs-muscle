396264e594739927f8d4f11b565867f201a2636a
Yuichi Kanno <fksmhr077@gmail.com>
2024-08-24 14:15:48 +0900

    fix prisma

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -6,17 +6,18 @@ import { PrismaClient } from "@prisma/client";
 let prisma: PrismaClient;
 
 // TODO: productionの場合の対応
-if (process.env.NODE_ENV === "production") {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  // prisma = new PrismaClient();
-  prisma = global.prisma;
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+// if (process.env.NODE_ENV === "production") {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   // prisma = new PrismaClient();
+//   prisma = global.prisma;
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+prisma = new PrismaClient();
 
 export default prisma;
